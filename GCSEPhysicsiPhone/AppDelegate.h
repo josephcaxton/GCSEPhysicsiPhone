e15cff8262d76a1f3fec4dacc60b8ecf4b409cc9
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2012-08-09 09:38:17 +0100

    Initial Commit

---
+++ GCSEPhysicsiPhone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCSEPhysicsiPhone
+//
+//  Created by Joseph caxton-Idowu on 09/08/2012.
+//  Copyright (c) 2012 caxtonidowu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
